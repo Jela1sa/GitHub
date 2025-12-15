6f8436aa4298f904c030c7f5434d02f56802bf7b
Fenke van de Reep <732176@student.inholland.nl>
2025-12-15 15:23:06 +0100

    meeeep

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("hello party people");
+            Console.WriteLine("Beep Beep people!!!!");
             Console.WriteLine("MEEP MEEP");
         }
     }
